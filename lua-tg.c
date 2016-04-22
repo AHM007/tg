387a082aacadafb5f919e9cb3ee5acfb7103304e
@Behrooz Yaghi <iranchild1@gmail.com>
2016-04-22 14:52:38 +0430

    this is super group block funcation

--- lua-tg.c
+++ lua-tg.c
@@ -9,7 +9,7 @@
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
     You should have received a copy of the GNU General Public License
-    along with this telegram-cli.  If not, see <http://www.gnu.org/licenses/>.
+    along with this telegram-cli.  If not,  see <http://www.gnu.org/licenses/>.
     Copyright Vitaly Valtman 2013-2015
 */
 
